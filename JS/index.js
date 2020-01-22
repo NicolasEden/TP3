a3f6e9811c2f4f01edf66fd6ab3fe666bd91f549
NefiteTifall <nefitetifall@gmail.com>
2020-01-22 12:45:38 +0100

    no message

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@ var listIMG = null;
 var donne = null;
 var core = document.body.innerHTML;
 function getIMG(word, number){
-    document.body.innerHTML = core;
+    document.getElementById('content-img').innerHTML = "";
     $.ajax({
         url: 'https://api.unsplash.com/search/photos?client_id=82c3342c7766b1900717e0a7577f3303cea19fa72c1d4aba79c7a90638eb69b5&page=1&query='+word+'&quantity=100&h=200&w=270',
         type: 'GET',
@@ -35,4 +35,20 @@ function getIMG(word, number){
 
 document.body.addEventListener('load', function(event) {
     getIMG('all', 2)
+});
+
+document.getElementById('all').addEventListener('click', function(event) {
+    getIMG('all', 2)
+});
+document.getElementById('Design').addEventListener('click', function(event) {
+    getIMG('Design', 2)
+});
+document.getElementById('Code').addEventListener('click', function(event) {
+    getIMG('Code', 2)
+});
+document.getElementById('Photography').addEventListener('click', function(event) {
+    getIMG('Photography', 2)
+});
+document.getElementById('Apps').addEventListener('click', function(event) {
+    getIMG('Apps', 2)
 });
